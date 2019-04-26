22f15b774876f50b07ac31392e06993b00517d35
jaunbiance <github email address>
2019-04-26 14:02:33 -0400

    We did it git add .git add . with: @JsnGmz

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,24 @@
 class FundingRound
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_reader :startup, :venture_capitalist, :startup, :investment
+  attr_accessor :type
+
+  def initialize(venture_cap, startup, type, invest)
+    @venture_capitalist = venture_cap
+    @startup = startup
+    @type = type
+    @investment = invest >= 0 ? invest.to_f : 0.0#.investment
+    self.class.all << self
+  end
+
+  # def investment
+  #   @investment >= 0 ? @investment.to_f : "Error"
+  # end
+
 
 end
