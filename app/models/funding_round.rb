4ff6d4e83b2f2b20d1f01a201633257f5c629410
Jake <jakelovitz@gmail.com>
2019-04-26 14:03:46 -0400

    @oscei303, @qunli02 and I solved the Silicon Valley code challenge

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,28 @@
 class FundingRound
 
+  attr_reader :startup, :venture_capitalist, :type
+  attr_accessor :investment_amount
+
+  @@all = []
+
+  def initialize(startup, venture_capitalist, type, amount)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment_amount = amount
+    @@all << self #this may need to change later
+  end
+
+  def investment
+    if self.investment_amount < 0
+      self.investment_amount = 0
+    else
+      self.investment_amount
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
 end
